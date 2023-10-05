a9a07da7c1ec6fc04dea9beab6903cf0b72393f0
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-10-05 11:10:38 +0200

    fix: env vars

--- .github/scripts/set-version.sh
+++ .github/scripts/set-version.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 # REQUIRES:
-# - GITHUB_REF ${{ github.ref }}
+# - GITHUB_REF_TYPE ${{ env.GITHUB_REF_TYPE }} - tag or branch
+# - GITHUB_REF_NAME ${{ env.GITHUB_REF_NAME }}
 # - GITHUB_SHA ${{ github.sha }}
 # - GITHUB_ENV
 
 is_tag="false"
 
-echo "GITHUB_REF: $GITHUB_REF"
+echo "GITHUB_REF_TYPE: $GITHUB_REF_TYPE"
 
-if [[ "$GITHUB_REF" == "tag" ]]; then
+if [[ "$GITHUB_REF_TYPE" == "tag" ]]; then
     is_tag="true"
 fi
 
 if [[ $is_tag == "true" ]]; then
-  echo "app version: $GITHUB_REF"
-  echo "APP_VERSION=$GITHUB_REF" >> "$GITHUB_ENV"
+  echo "app version: $GITHUB_REF_NAME"
+  echo "APP_VERSION=$GITHUB_REF_NAME" >> "$GITHUB_ENV"
 else
   sha_full="$GITHUB_SHA"
   version="${sha_full:0:7}"
